7de31da401128a2e296e7b4f9df563929010282b
Diego <diegorealpet@gmail.com>
2025-02-19 18:22:11 -0600

    Trying to use userdata

---
+++ assets/userData.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+sudo yum install -y java-17-amazon-corretto
+java -version
+systemctl status amazon-ssm-agent
+echo "Userdata script exit"
